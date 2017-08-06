a86ad76e93a18328af6b12549b0b2b48f38fd401
Park Vac <parkvac@hotmail.com>
2017-08-06 22:22:19 +1200

    Up to Lecture 8

---
+++ ParkVac/Controllers/CarparksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ParkVac.Models;
+
+namespace ParkVac.Controllers
+{
+    public class CarparksController : Controller
+    {
+        // GET: Carparks
+        public ActionResult Random()
+        {
+            var carpark = new Carpark() { Name = "HP" };
+            return View(carpark);
+        }
+    }
+}
